563b791f707685324df5379648a1c29452857145
andreas <andreas.kuendig@gmx.ch>
2020-08-02 01:04:02 +0200

    play notes

--- main.js
+++ main.js
@@ -1,32 +1,59 @@
+// file:///home/pi/work/webo/index.html
+const oscillators = {};
+let audioCtx = undefined;
+
+const keyToFrequency = {a:440, b:660, c:880};
+
+const createOsc = (frequency) => {
+  initializeAudioContext();
+  let osc = audioCtx.createOscillator();
+  osc.type = 'sine';
+  // osc.setPeriodicWave(wave);
+  osc.frequency.value = frequency;
+  osc.connect(audioCtx.destination);
+  return osc;
+};
+
+const initializeAudioContext = () => {
+  if(!audioCtx){
+    const AudioContext = window.AudioContext || window.webkitAudioContext;
+    audioCtx = new AudioContext();
+  }
+};
+
 const start = () => {
-  const AudioContext = window.AudioContext || window.webkitAudioContext;
-  const audioCtx = new AudioContext();
   //let wave = audioCtx.createPeriodicWave(wavetable.real, wavetable.imag);
-  const createOsc = (frequency, audioCtx) => {
-    let osc = audioCtx.createOscillator();
-    osc.type = 'sine';
-    // osc.setPeriodicWave(wave);
-    osc.frequency.value = frequency;
-    osc.connect(audioCtx.destination);
-    return osc;
-  };
   const playSweep = () => {
     let osc1 = createOsc(440, audioCtx);
     let osc2 = createOsc(880, audioCtx);
     let now = audioCtx.currentTime;
     osc1.start();
     osc2.start();
-    osc1.stop(now + 1 );
-    osc2.stop(now + 1 );
-    // osc1 = createOsc(440, audioCtx);
-    // now = audioCtx.currentTime;
-    // osc1.start();
-    // osc1.stop(now + 1 );
-    // osc2 = createOsc(880, audioCtx);
-    // now = audioCtx.currentTime;
-    // osc2.start();
-    // osc2.stop(now + 1 );
+    osc1.stop(now + 1);
+    osc2.stop(now + 1);
   };
 
   window.p = playSweep;
 };
+document.addEventListener('DOMContentLoaded', () => {
+  'use strict';
+  start();
+  document.addEventListener('keydown', event => {
+    const  {key} = event;
+    const oldOsc = oscillators[key];
+    const frequency = keyToFrequency[key];
+    if(oldOsc || !frequency) { return; }
+    console.log('down', key);
+    const newOsc = createOsc(frequency, audioCtx);
+    newOsc.start();
+    oscillators[key] = newOsc;
+  });
+  document.addEventListener('keyup', event => {
+    const  {key} = event;
+    const oldOsc = oscillators[key];
+    if(!oldOsc){ return; }
+    console.log('up', key);
+    oldOsc.stop(audioCtx.now);
+    delete oscillators[key];
+  });
+});
