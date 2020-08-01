7b840cfaa7a21d765492984ff9f7b4337f8ce01d
andreas <andreas.kuendig@gmx.ch>
2020-08-01 02:22:11 +0200

    second osc

--- main.js
+++ main.js
@@ -1,17 +1,32 @@
 const start = () => {
   const AudioContext = window.AudioContext || window.webkitAudioContext;
   const audioCtx = new AudioContext();
-  let wave = audioCtx.createPeriodicWave(wavetable.real, wavetable.imag);
+  //let wave = audioCtx.createPeriodicWave(wavetable.real, wavetable.imag);
+  const createOsc = (frequency, audioCtx) => {
+    let osc = audioCtx.createOscillator();
+    osc.type = 'sine';
+    // osc.setPeriodicWave(wave);
+    osc.frequency.value = frequency;
+    osc.connect(audioCtx.destination);
+    return osc;
+  };
+  const playSweep = () => {
+    let osc1 = createOsc(440, audioCtx);
+    let osc2 = createOsc(880, audioCtx);
+    let now = audioCtx.currentTime;
+    osc1.start();
+    osc2.start();
+    osc1.stop(now + 1 );
+    osc2.stop(now + 1 );
+    // osc1 = createOsc(440, audioCtx);
+    // now = audioCtx.currentTime;
+    // osc1.start();
+    // osc1.stop(now + 1 );
+    // osc2 = createOsc(880, audioCtx);
+    // now = audioCtx.currentTime;
+    // osc2.start();
+    // osc2.stop(now + 1 );
+  };
 
-    const playSweep = () =>{
-        let osc = audioCtx.createOscillator();
-        osc.type='sine';
-        // osc.setPeriodicWave(wave);
-        osc.frequency.value = 440;
-        osc.connect(audioCtx.destination);
-        osc.start();
-        osc.stop(audioCtx.currentTime + 1/2);
-    };
-
-    window.p = playSweep;
+  window.p = playSweep;
 };
